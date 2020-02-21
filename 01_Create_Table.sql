65155367b75fb497fc620100ea402b9d989a357a
Daniel Santos Pereira <danielsantosjc@live.com>
2020-02-21 12:53:59 -0300

    Add files via upload

---
+++ 01_Create_Table.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA escritores;
+
+CREATE SEQUENCE escritores.IDEscritor;
+CREATE TABLE escritores.Escritor(
+  IDEscritor int default nextval('escritores.IDEscritor'::regclass) PRIMARY KEY,
+  Nome Varchar(50) not null,
+  DataNascimento Date not null,
+  DataFalecimento Date
+);
